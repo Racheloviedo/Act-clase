4756ef5795731d1b9cabaca5868f3a10f2ba396f
Rachel Oviedo <00033125@uca.edu.sv>
2025-04-28 12:28:13 -0600

    Add files via upload

---
+++ Ejercicio-1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int number;
+    int a;
+    int b;
+    cout<<"insert a number for a";
+    cin>>a;
+    cout<<"insert a number for b";
+    cin>>b;
+    if (a<=100 )
+    {
+        /* code */
+    }
+    
+
+    
+    
+   
+
+
+     
+
+
+
+    return 0;
+}
